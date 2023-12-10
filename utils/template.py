5cd143fcb5eefcb68b383508754c336c333acb8b
augustin64 <me.git@augustin64.fr>
2023-12-10 15:24:38 +0100

    Update template.py

--- utils/template.py
+++ utils/template.py
@@ -2,10 +2,12 @@
 """
 Jour DAY du défi Advent Of Code pour l'année YEAR
 """
+import os
 
 def read_sample():
     """récupère les entrées depuis le fichier texte correspondant"""
-    with open('inputs/dayDAY.txt', 'r') as f:
+    filename = os.path.join(os.path.dirname(__file__ ), "inputs", "dayDAY.txt")
+    with open(filename, 'r') as f:
         sample = f.read().split('\n')
     sample = [ i for i in sample if i != '' ]
     return sample
